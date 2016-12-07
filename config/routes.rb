bc18e7fd2674da9e970ef974bcb5880b55eb538d
Zoheb Salam <zoheb_391@hotmail.com>
2016-12-07 15:27:35 +0000

    edit and update tests working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'grams#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  resources :grams
+  resources :grams, only: [:new,:create,:show, :edit, :update]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -70,4 +70,48 @@ RSpec.describe GramsController, type: :controller do
 
   end
 
+  describe "grams#edit action" do
+    it "should successfully show the edit form if the gram is found" do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      gram = FactoryGirl.create(:gram)
+
+      get :edit, id: gram.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error message if the gram is not found" do
+      gram = FactoryGirl.create(:gram)
+
+      get :edit, id: 'tacosaus'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "grams#update action" do
+    it "should successfully update the gram" do
+      gram = FactoryGirl.create(:gram, message: 'initial Value')
+      
+      patch :update, id: gram.id, gram: {message: 'changed'}
+      expect(response).to redirect_to root_path
+      gram.reload
+      expect(gram.message).to eq "changed"
+    end
+    
+    it "should show 404 if gram not found" do
+      patch :update, id: "YOLOSWAG", gram: {message: 'Changed'}
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should render the edit form with an http status of unprocessable_entity" do
+      gram = FactoryGirl.create(:gram, message: "initial Value")
+      
+      patch :update, id: gram.id, gram: { message: '' }
+      expect(response).to have_http_status(:unprocessable_entity)
+      gram.reload
+      expect(gram.message).to eq "initial Value"
+    end
+ 
+  end
+
 end
